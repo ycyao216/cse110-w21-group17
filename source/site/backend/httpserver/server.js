4bf0a8361e23c8b4df5f2126635d165ac8187cf5
Bo <b1su@ucsd.edu>
2021-02-08 23:55:42 +0800

    Add settings.html

--- source/site/backend/httpserver/server.js
+++ source/site/backend/httpserver/server.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: "./frontend" });
+    res.sendFile('index.html', {
+        root: "./frontend"
+    });
 });
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
