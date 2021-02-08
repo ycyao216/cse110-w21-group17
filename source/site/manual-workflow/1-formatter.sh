b64b70be395a4b02772e8e5f8dc75f641a73ed05
Bo <b1su@ucsd.edu>
2021-02-08 22:27:52 +0800

    add html validator

--- source/site/manual-workflow/1-formatter.sh
+++ source/site/manual-workflow/1-formatter.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
-export jsbeautify="../node_modules/.bin/js-beautify"
-find ../backend -type f -name "*.html" -exec $jsbeautify -r {} \;
-find ../backend -type f -name "*.js" -exec $jsbeautify -r {} \;
-find ../backend -type f -name "*.css" -exec $jsbeautify -r {} \;
-find ../frontend -type f -name "*.html" -exec $jsbeautify -r {} \;
-find ../frontend -type f -name "*.js" -exec $jsbeautify -r {} \;
-find ../frontend -type f -name "*.css" -exec $jsbeautify -r {} \;
+cd ../
+export jsbeautify="node_modules/.bin/js-beautify"
+find backend -type f -name "*.html" -exec $jsbeautify -r {} \;
+find backend -type f -name "*.js" -exec $jsbeautify -r {} \;
+find backend -type f -name "*.css" -exec $jsbeautify -r {} \;
+find frontend -type f -name "*.html" -exec $jsbeautify -r {} \;
+find frontend -type f -name "*.js" -exec $jsbeautify -r {} \;
+find frontend -type f -name "*.css" -exec $jsbeautify -r {} \;
