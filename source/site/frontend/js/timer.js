eb2758ba50cb31cee09d49ee41fca445a0439212
hcjyou <hcyou@ucsd.edu>
2021-02-14 15:59:11 -0800

    timer.js ver 1

--- source/site/frontend/js/timer.js
+++ source/site/frontend/js/timer.js
@@ -1,3 +1,6 @@
+// interval for countdown
+var everySecond;
+
 /*
 * function countdown(HTMLElement)
 *
@@ -5,7 +8,7 @@
 * Use: This function takes in the html element for the timer, grabs the number from it,
 * decrements it by 1, and changes the innerHTML of the element.
 */
-function countdown(timerElement) {
+function countdownElement(timerElement) {
 
     // should be something like 25:00
     let text = timerElement.innerHTML;
@@ -37,14 +40,83 @@ function countdown(timerElement) {
     //set the innerHTML back
     text = minutes.toString + ":" + seconds.toString;
 
+    if(text === "00:00" && everySecond != null) {
+        clearInterval(everySecond);
+    }
+
     timerElement.innerHTML = text;
 
     // for testing purposes
     return text;
 }
 
+/* 
+ * function altType(Number)
+ *
+ * param: type - a number (0 or 1) representing whether timer is on break or not
+ * 0 = pomodoro
+ * 1 = break
+ */
+function altType(type) {
+    if(type) {
+        type = 0;
+    } else {
+        type = 1;
+    }
+    return type;
+}
+
+/*
+ * function isLongBreak(Number)
+ * 
+ * param: numOfPomo - the number of pomodoros since pressing start
+ * every 4 pomos returns true for long break
+ */
+function isLongBreak(numOfPomo) {
+    if(numOfPomo % 4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/*
+ * function startButton ()
+ * 
+ * param: HTMLElement of timer to pass to countdown
+ * function to begin the countdown for the timer, should return when
+ * timer reaches 0. resetting of timer and breaks handled in driver.
+ */
+function startButton(timerElement) {
+
+    everySecond = setInterval(countdownElement(timerElement), [1000]);
+
+}
+
+/*
+ * function stopEarlyButton()
+ * 
+ * param: HTMLelement of prompt to unhide (display stlye should be "none")
+ * function to bring up the prompts when user finishes early. timer keeps going
+ */
+function stopEarlyButton(earlyPrompt) {
+    earlyPrompt.style.display = "block";
+}
+
+/*
+ * function emergStopButton()
+ *
+ * param: HTMLElement of timer to set to 00:00
+ * function to instantly stop timer. should keep old # of cycles.
+ */
+function emergStopButton(timerElement) {
+    timerElement.innerHTML = "00:00";
+}
+
 // for testing
 module.exports = {
-    countdown
+    countdownElement,
+    isLongBreak,
+    altType
 };
 
--- source/site/frontend/js/timer.test.js
+++ source/site/frontend/js/timer.test.js
@@ -1,25 +1,44 @@
 const timer = require('./timer');
 let testobj1 = document.createElement("p");
-testobj.innerHTML = "25:00";
+testobj1.innerHTML = "25:00";
 let testobj2 = document.createElement("p");
-testobj.innerHTML = "22:37";
+testobj2.innerHTML = "22:37";
 let testobj3 = document.createElement("p");
-testobj.innerHTML = "22:00";
+testobj3.innerHTML = "22:00";
 let testobj4 = document.createElement("p");
-testobj.innerHTML = "00:00";
+testobj4.innerHTML = "00:00";
 
+//countdownElement
 test('pass html object with 25:00', () => {
-    expect(countdown(testobj1)).toBe("24:59");
+    expect(countdownElement(testobj1)).toBe("24:59");
 });
 
 test('pass html object with 22:37', () => {
-    expect(countdown(testobj2)).toBe("22:36");
+    expect(countdownElement(testobj2)).toBe("22:36");
 });
 
 test('pass html object with 22:00', () => {
-    expect(countdown(testobj3)).toBe("21:59");
+    expect(countdownElement(testobj3)).toBe("21:59");
 });
 
 test('pass html object with 00:00', () => {
-    expect(countdown(testobj4)).toBe("00:00");
+    expect(countdownElement(testobj4)).toBe("00:00");
+});
+
+// isLongBreak
+test('pass 7', () => {
+    expect(isLongBreak(7)).toBe(false);
+});
+
+test('pass 8', () => {
+    expect(isLongBreak(8)).toBe(true);
+});
+
+// altType
+test('pass number representing pomdodoro', () => {
+    expect(altType(0)).toBe(1);
+});
+
+test('pass number representing break', () => {
+    expect(altType(1)).toBe(0);
 });
