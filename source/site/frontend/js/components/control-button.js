b463eef12c4deb22dddd48c07cfed7ce23266e5d
DanielChenOfficial2 <53456446+DanielChenOfficial2@users.noreply.github.com>
2021-03-01 23:12:00 -0800

    Added variables and getters for shadow and document

--- source/site/frontend/js/components/control-button.js
+++ source/site/frontend/js/components/control-button.js
@@ -1,5 +1,5 @@
 // TODO Ready For Testing
-export function define_control_button(html) {
+function define_control_button(html) {
     class CControlButton extends HTMLElement {
         constructor() {
             super();
@@ -9,7 +9,8 @@ export function define_control_button(html) {
             shadow.innerHTML = html;
 
             let document = this.shadowRoot;
-
+            this.shadow = shadow;
+            this.document = document;
             function _class(name) {
                 return document.querySelectorAll("." + name);
             }
@@ -17,7 +18,18 @@ export function define_control_button(html) {
             // Use value in the custom tag to be the display text in the button
             document.getElementById('button').innerText = this.textContent
         }
+        get_shadow() {
+            return this.shadow;
+        }
+        get_document() {
+            return this.document;
+        }
     }
     customElements.define('c-control-button', CControlButton);
     return CControlButton;
 }
+
+// for testing
+module.exports = {
+    define_control_button,
+};
